ee097b70005b71910969e74b39ddacedcb5b0094
Miten Gajjar <miten3377@gmail.com>
2020-11-24 18:55:29 +0530

    added oppertunitypage

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import NavBar from './components/navbar/Navbar';
 import HomePage from './components/homepage/HomePage';
 import EventPage from './components/eventpage/eventpage';
 import VisionPage from './components/visionpage/visionpage';
+import OppertunityPage from './components/oppertunitypage/oppertunitypage';
 
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
         <HomePage />
         <EventPage />
         <VisionPage/>
+        <OppertunityPage/>
       </div>
     </BrowserRouter>
   );
