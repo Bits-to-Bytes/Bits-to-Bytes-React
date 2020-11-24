398c6235d8541f19af0e57214d1d5d09a28c3339
Miten Gajjar <miten3377@gmail.com>
2020-11-24 18:16:13 +0530

    added visionpage make it responsive

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import { BrowserRouter, Route } from 'react-router-dom'
 
 import './App.css';
 
-import NavBar from './components/navbar/Navbar'
+import NavBar from './components/navbar/Navbar';
 import HomePage from './components/homepage/HomePage';
-import EventPage from './components/eventpage/eventpage'
+import EventPage from './components/eventpage/eventpage';
+import VisionPage from './components/visionpage/visionpage';
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
         <Route exact path="#home-page" component={HomePage}></Route>
         <HomePage />
         <EventPage />
+        <VisionPage/>
       </div>
     </BrowserRouter>
   );
