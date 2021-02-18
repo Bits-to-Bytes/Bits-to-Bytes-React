78ea2fd4a9ef41528b9db17fc86e722906b2fc72
Miten Gajjar <miten3377@gmail.com>
2021-02-18 21:06:07 +0530

    updated event for DSA

--- src/components/blogspage/blogspage.js
+++ src/components/blogspage/blogspage.js
@@ -6,8 +6,8 @@ const BlogsPage = () =>{
         <div>
             <h1 className="title"> Blogs </h1>  
             <div className="blogs-page" id="blogs">
-            <BlogCard image="./assets/blogs/git.jpeg" title="Launch Event of Bits To Bytes" info="In this blog, We will talk about the launch event of the Bits to Bytes programming club of GEC Bhavnagar.." link="https://medium.com/bitstobytes/launch-event-of-bits-to-bytes-f6fb6d1d7999" />
-            <BlogCard image="./assets/blogs/launch.png" title="Learn Git and Github in 5 minutes" info="In this blog,We will learn to use Git and  Github and more about open-source and version control.." link="https://medium.com/bitstobytes/learn-git-and-github-in-5-minutes-1f8047839e2"/>
+            <BlogCard image="./assets/blogs/launch.png" title="Launch Event of Bits To Bytes" info="In this blog, We will talk about the launch event of the Bits to Bytes programming club of GEC Bhavnagar.." link="https://medium.com/bitstobytes/launch-event-of-bits-to-bytes-f6fb6d1d7999" />
+            <BlogCard image="./assets/blogs/git.jpeg" title="Learn Git and Github in 5 minutes" info="In this blog,We will learn to use Git and  Github and more about open-source and version control.." link="https://medium.com/bitstobytes/learn-git-and-github-in-5-minutes-1f8047839e2"/>
         </div>
         </div>
     );
--- src/components/eventpage/event.js
+++ src/components/eventpage/event.js
@@ -2,23 +2,45 @@ import React from "react";
 import "./eventpage.css";
 
 const Event = () => {
-  return (
-    <div className="event">
-      <div className="event-img">
-        <img src="./assets/events/react.svg" alt="reactjs" />
-      </div>
-      <div className="info">
-        <h1 className="title">React JS Webinar</h1>
-        <p className="about-event">
-          React is an open-source, front end, JavaScript library for building
-          user interfaces or UI components. It is maintained by Facebook and a
-          community of individual developers and companies. React can be used as
-          a base in the development of single-page or mobile applications
-        </p>
-      </div>
-     
-    </div>
-  );
+    return (
+        <div className="event">
+            <div className="event-img">
+                <lottie-player
+                    src="https://assets3.lottiefiles.com/packages/lf20_ZmsQVB.json"
+                    mode="bounce"
+                    background="transparent"
+                    speed="1"
+                    style={{ width: "100%", height: "100%" }}
+                    loop
+                    autoplay
+                ></lottie-player>
+            </div>
+            <div className="info">
+                <h1 className="title">Data Structures and Algorithms</h1>
+                <p className="about-event">
+                    Data structure and algorithms help in understanding the
+                    nature of the problem at a deeper level and thereby a better
+                    understanding of the world. Data structures and algorithms
+                    (DSA) goes through solutions to standard problems in detail
+                    and gives you an insight into how efficient it is to use
+                    each one of them. It also teaches you the science of
+                    evaluating the efficiency of an algorithm. This enables you
+                    to choose the best of various choices.
+                </p>
+                <a
+                    href="https://join.slack.com/t/bits-to-bytesstudents/shared_invite/zt-mhkyjgpx-iuC4iuXsQdJ1Ekxz20yoDA"
+                    className="event-button"
+                    target="_blank"
+                    rel="noreferrer"
+                >
+                    <button>
+                        <p>Join Slack</p>
+                        <i className="fas fa-arrow-right"></i>
+                    </button>
+                </a>
+            </div>
+        </div>
+    );
 };
 
 export default Event;
