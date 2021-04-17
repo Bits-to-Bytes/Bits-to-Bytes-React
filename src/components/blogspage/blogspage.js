685b80f787340ed17a1d2d73fe9aea26ae16ee5f
Neel Gorasiya <mgorasiya1974@gmail.com>
2021-04-17 14:34:39 +0530

    Made changes with margins and alignments in only home,event and blog pages

--- src/components/blogspage/blogspage.js
+++ src/components/blogspage/blogspage.js
@@ -1,17 +1,28 @@
-import BlogCard from './blogcard';
-import './blogspage.css';
+import BlogCard from "./blogcard";
+import "./blogspage.css";
 
-const BlogsPage = () =>{
-    return(
-        <div>
-            <h1 className="title"> Blogs </h1>  
-            <div className="blogs-page" id="blogs">
-            <BlogCard image="./assets/blogs/launch.png" title="Launch Event of Bits To Bytes" info="In this blog, We will talk about the launch event of the Bits to Bytes programming club of GEC Bhavnagar.." link="https://medium.com/bitstobytes/launch-event-of-bits-to-bytes-f6fb6d1d7999" />
-            <BlogCard image="./assets/blogs/git.jpeg" title="Learn Git and Github in 5 minutes" info="In this blog,We will learn to use Git and  Github and more about open-source and version control.." link="https://medium.com/bitstobytes/learn-git-and-github-in-5-minutes-1f8047839e2"/>
+const BlogsPage = () => {
+  return (
+    <div className="blogsPage">
+      <h1 className="blogsPage-title"> Blogs </h1>
+      <div className="blogs-page" id="blogs">
+        <div className="cards">
+          <BlogCard
+            image="./assets/blogs/launch.png"
+            title="Launch Event of Bits To Bytes"
+            info="In this blog, We will talk about the launch event of the Bits to Bytes programming club of GEC Bhavnagar.."
+            link="https://medium.com/bitstobytes/launch-event-of-bits-to-bytes-f6fb6d1d7999"
+          />
+          <BlogCard
+            image="./assets/blogs/git.jpeg"
+            title="Learn Git and Github in 5 minutes"
+            info="In this     blog,We will learn to use Git and  Github and more about open-source and version control.."
+            link="https://medium.com/bitstobytes/learn-git-and-github-in-5-minutes-1f8047839e2"
+          />
         </div>
-        </div>
-    );
-}
-
+      </div>
+    </div>
+  );
+};
 
 export default BlogsPage;
